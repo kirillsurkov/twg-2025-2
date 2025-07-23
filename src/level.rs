e39a745973b3fca5f80c3a106a12d9d1a5d1bb05
Surkov, Kirill <kirill.surkov@intel.com>
2025-07-23 22:14:15 +0300

    Add biomes

--- src/level.rs
+++ src/level.rs
@@ -1,11 +1,19 @@
 use std::{
-    cmp::Reverse,
     collections::{BinaryHeap, HashSet},
-    f64::consts::E,
+    f32::consts::E,
+    ops::Div,
 };
 
-use bevy::prelude::*;
+use bevy::{color::palettes::css::*, prelude::*};
+use bevy_heightmap::HeightMap;
 use fast_poisson::Poisson2D;
+use imageproc::{
+    distance_transform::euclidean_squared_distance_transform,
+    drawing::{draw_filled_rect_mut, draw_line_segment_mut},
+    filter,
+    image::{GrayImage, ImageBuffer, Luma, LumaA, Pixel, Primitive, Rgb, Rgba},
+    rect,
+};
 use kiddo::{KdTree, SquaredEuclidean};
 use petgraph::{
     Graph, Undirected,
@@ -13,6 +21,7 @@ use petgraph::{
     data::Element,
     visit::{EdgeRef, IntoNodeReferences},
 };
+use rand::distr::{Distribution, weighted::WeightedIndex};
 
 fn delaunay(points: &Vec<Vec2>) -> Graph<(), f32, Undirected> {
     let mut graph = Graph::new_undirected();
@@ -103,36 +112,49 @@ fn graph(points: &Vec<Vec2>, ratio: f32) -> Vec<(usize, usize)> {
     edges.into_iter().collect()
 }
 
+pub enum LevelBiome {
+    Red,
+    Green,
+    Blue,
+    Cyan,
+    Magenta,
+    Yellow,
+    Orange,
+    Purple,
+}
+
 pub struct LevelPart {
     points: Vec<Vec2>,
     edges: Vec<(usize, usize)>,
     bounds: Rect,
+    pub radius: f32,
+    biome: LevelBiome,
 }
 
-impl LevelPart {}
-
 pub struct LevelPartBuilder {
-    width: f64,
-    height: f64,
+    width: f32,
+    height: f32,
     count: usize,
     fill_ratio: f32,
+    biome: LevelBiome,
 }
 
 impl LevelPartBuilder {
     const GAP: Vec2 = Vec2::new(1.0, 1.0);
 
-    pub fn new() -> Self {
+    pub fn new(biome: LevelBiome) -> Self {
         Self {
             width: 0.0,
             height: 0.0,
             count: 0,
             fill_ratio: 0.0,
+            biome,
         }
     }
 
     pub fn with_size(mut self, width: f32, height: f32) -> Self {
-        self.width = width as f64;
-        self.height = height as f64;
+        self.width = width;
+        self.height = height;
         self
     }
 
@@ -146,13 +168,14 @@ impl LevelPartBuilder {
         self
     }
 
-    fn estimate_radius(&self) -> f64 {
-        (2.0 * self.width * self.height / (E * self.count as f64)).sqrt()
+    fn estimate_radius(&self) -> f32 {
+        (2.0 * self.width * self.height / (E * self.count as f32)).sqrt()
     }
 
-    pub fn build(&self) -> LevelPart {
+    pub fn build(self) -> LevelPart {
+        let radius = self.estimate_radius();
         let points = Poisson2D::new()
-            .with_dimensions([self.width, self.height], self.estimate_radius())
+            .with_dimensions([self.width as f64, self.height as f64], radius as f64)
             .iter()
             .map(|[x, y]| Vec2 {
                 x: x as f32 - 0.5 * self.width as f32,
@@ -171,6 +194,8 @@ impl LevelPartBuilder {
             points,
             edges,
             bounds,
+            radius,
+            biome: self.biome,
         }
     }
 }
@@ -184,70 +209,87 @@ pub enum PartAlign {
 
 pub struct Level {
     kd: KdTree<f32, 2>,
-    bounds: Vec<Rect>,
-    points: Vec<Vec2>,
+    bounds: Rect,
+    parts: Vec<LevelPart>,
+    points: Vec<(usize, Vec2)>,
     edges: Vec<(usize, usize)>,
 }
 
 impl Level {
+    const BLACK: Luma<u8> = Luma([0]);
+    const WHITE: Luma<u8> = Luma([255]);
+
     pub fn new() -> Self {
         Self {
             kd: KdTree::new(),
-            bounds: vec![],
+            bounds: Rect {
+                min: Vec2::MAX,
+                max: Vec2::MIN,
+            },
+            parts: vec![],
             points: vec![],
             edges: vec![],
         }
     }
 
-    pub fn add(&mut self, offset: Vec2, part: LevelPart) -> usize {
+    pub fn add(&mut self, offset: Vec2, mut part: LevelPart) -> usize {
         let idx_offset = self.points.len();
         let points = part
             .points
-            .into_iter()
+            .iter()
             .enumerate()
             .map(|(i, p)| (i + idx_offset, p + offset))
             .collect::<Vec<_>>();
 
+        part.bounds = Rect {
+            min: part.bounds.min + offset,
+            max: part.bounds.max + offset,
+        };
+
+        self.bounds = Rect::new(
+            self.bounds.min.x.min(part.bounds.min.x),
+            self.bounds.min.y.min(part.bounds.min.y),
+            self.bounds.max.x.max(part.bounds.max.x),
+            self.bounds.max.y.max(part.bounds.max.y),
+        );
+
         self.edges.extend(
             part.edges
-                .into_iter()
+                .iter()
                 .map(|(start, end)| (start + idx_offset, end + idx_offset)),
         );
 
-        let mut heap = points
+        let closest = points
             .iter()
             .flat_map(|(i, p)| {
                 self.kd
                     .nearest_n::<SquaredEuclidean>(&[p.x, p.y], 2)
                     .into_iter()
-                    .map(move |n| Reverse((n, *i)))
+                    .map(move |n| (n, *i))
             })
-            .collect::<BinaryHeap<_>>();
+            .collect::<BinaryHeap<_>>()
+            .into_sorted_vec();
 
-        for _ in 0..2 {
-            if let Some(Reverse((neighbour, i))) = heap.pop() {
-                self.edges.push((i, neighbour.item as usize))
-            }
+        println!("{closest:#?}");
+
+        for (neighbour, i) in closest.into_iter().take(2) {
+            self.edges.push((i, neighbour.item as usize));
         }
 
         for (i, point) in &points {
             self.kd.add(&[point.x, point.y], *i as u64);
         }
 
-        self.points.extend(points.into_iter().map(|(_, p)| p));
-
-        self.bounds.push(Rect {
-            min: part.bounds.min + offset,
-            max: part.bounds.max + offset,
-        });
-
-        self.bounds.len() - 1
+        let id = self.parts.len();
+        self.points.extend(points.into_iter().map(|(_, p)| (id, p)));
+        self.parts.push(part);
+        id
     }
 
     pub fn add_after(&mut self, after: usize, align: PartAlign, part: LevelPart) -> usize {
-        let after = self.bounds[after];
-        let edge = 0.5 * (after.size() + part.bounds.size());
-        let offset = after.center() - part.bounds.center();
+        let after = &self.parts[after];
+        let edge = 0.5 * (after.bounds.size() + part.bounds.size());
+        let offset = after.bounds.center() - part.bounds.center();
         let offset = match align {
             PartAlign::Left => offset - Vec2::new(edge.x, 0.0),
             PartAlign::Right => offset + Vec2::new(edge.x, 0.0),
@@ -257,17 +299,298 @@ impl Level {
         self.add(offset, part)
     }
 
-    pub fn points(&self) -> impl Iterator<Item = Vec2> {
-        self.points.iter().cloned()
+    pub fn points(&self) -> impl Iterator<Item = (&LevelPart, Vec2)> {
+        self.points
+            .iter()
+            .map(|(i, p)| (&self.parts[*i], p.clone()))
     }
 
     pub fn edges(&self) -> impl Iterator<Item = (Vec2, Vec2)> {
         self.edges
             .iter()
-            .map(|(start, end)| (self.points[*start], self.points[*end]))
+            .map(|(start, end)| (self.points[*start].1, self.points[*end].1))
     }
 
-    pub fn bounds(&self) -> impl Iterator<Item = Rect> {
-        self.bounds.iter().cloned()
+    pub fn parts(&self) -> impl Iterator<Item = &LevelPart> {
+        self.parts.iter()
+    }
+
+    pub fn bounds(&self) -> Rect {
+        self.bounds
+    }
+
+    fn height_map(&self, scale: f32) -> ImageBuffer<Luma<f32>, Vec<f32>> {
+        let bounds = IRect {
+            min: (self.bounds.min * scale).as_ivec2(),
+            max: (self.bounds.max * scale).as_ivec2(),
+        };
+
+        let UVec2 {
+            x: width,
+            y: height,
+        } = bounds.size().as_uvec2();
+
+        let mut image = GrayImage::from_pixel(width, height, Self::BLACK);
+
+        // for point in &self.points {
+        //     let IVec2 { x, y } = (point * scale).as_ivec2() - bounds.min;
+        //     image.put_pixel(x as u32, y as u32, Self::WHITE);
+        // }
+
+        for (start, end) in self.edges() {
+            let start = start * scale - bounds.min.as_vec2();
+            let end = end * scale - bounds.min.as_vec2();
+            draw_line_segment_mut(&mut image, (start.x, start.y), (end.x, end.y), Self::WHITE);
+        }
+
+        let distances = euclidean_squared_distance_transform(&image);
+
+        filter::gaussian_blur_f32(
+            &ImageBuffer::from_fn(image.width(), image.height(), |x, y| {
+                Luma([distances.get_pixel(x, y).0[0].sqrt() as f32 / scale])
+            }),
+            2.0,
+        )
+    }
+
+    fn biome_map(&self, scale: f32) -> ImageBuffer<BiomePixel, Vec<f32>> {
+        let bounds = IRect {
+            min: (self.bounds.min * scale).as_ivec2(),
+            max: (self.bounds.max * scale).as_ivec2(),
+        };
+
+        let UVec2 {
+            x: width,
+            y: height,
+        } = bounds.size().as_uvec2();
+
+        let mut biomes =
+            ImageBuffer::<BiomePixel, Vec<f32>>::from_pixel(width, height, BiomePixel::default());
+
+        for part in &self.parts {
+            let IVec2 { x, y } = (part.bounds.min * scale).as_ivec2() - bounds.min;
+            let UVec2 {
+                x: width,
+                y: height,
+            } = (part.bounds.size() * scale).as_uvec2();
+
+            let mut pixel = BiomePixel([0.0; BiomePixel::CHANNEL_COUNT as usize]);
+            pixel.0[BiomePixel::RADIUS] = part.radius;
+            pixel.0[match part.biome {
+                LevelBiome::Red => BiomePixel::RED,
+                LevelBiome::Green => BiomePixel::GREEN,
+                LevelBiome::Blue => BiomePixel::BLUE,
+                LevelBiome::Cyan => BiomePixel::CYAN,
+                LevelBiome::Magenta => BiomePixel::MAGENTA,
+                LevelBiome::Yellow => BiomePixel::YELLOW,
+                LevelBiome::Orange => BiomePixel::ORANGE,
+                LevelBiome::Purple => BiomePixel::PURPLE,
+            }] = 1.0;
+
+            draw_filled_rect_mut(
+                &mut biomes,
+                rect::Rect::at(x, y).of_size(width, height),
+                pixel,
+            );
+        }
+
+        filter::gaussian_blur_f32(&biomes, 8.0)
+    }
+
+    pub fn terrain(&self, scale: f32) -> Mesh {
+        println!("heightmap");
+        let height_map = self.height_map(scale);
+        println!("biomemap");
+        let biome_map = self.biome_map(scale);
+        let bounds = (self.bounds.size() * scale).as_uvec2();
+
+        println!("mesh");
+        let mut mesh: Mesh = HeightMap {
+            size: bounds,
+            h: |coords| {
+                let coords = ((Vec2::new(coords.x, -coords.y) + 0.5) * bounds.as_vec2())
+                    .as_uvec2()
+                    .min(bounds - 1);
+                let dist = height_map.get_pixel(coords.x, coords.y).0[0];
+                let biome = biome_map.get_pixel(coords.x, coords.y).0;
+
+                let radius = biome[BiomePixel::RADIUS];
+
+                let road_width = 0.25 * radius;
+                let max_height = 0.5 * radius - road_width;
+
+                3.0 * (dist - road_width).max(0.0).div(max_height).powf(0.75)
+            },
+        }
+        .into();
+
+        mesh.compute_smooth_normals();
+
+        let choices = [RED, GREEN, BLUE, AQUA, MAGENTA, YELLOW, ORANGE, PURPLE];
+        let mut rng = rand::rng();
+        let mut colors = vec![];
+        for y in (0..biome_map.height()).rev() {
+            for x in 0..biome_map.width() {
+                let pixel = biome_map.get_pixel(x, y);
+                let dist = WeightedIndex::new(&pixel.0[1..=8]).unwrap();
+                colors.push(choices[dist.sample(&mut rng)].to_f32_array());
+            }
+        }
+        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+
+        mesh
+    }
+}
+
+#[derive(Clone, Copy)]
+struct BiomePixel([f32; Self::CHANNEL_COUNT as usize]);
+
+impl Default for BiomePixel {
+    fn default() -> Self {
+        let mut data = [0.0; Self::CHANNEL_COUNT as usize];
+        data[Self::RADIUS] = 1.0;
+        data[Self::RED] = 1.0;
+        Self(data)
+    }
+}
+
+impl BiomePixel {
+    const RADIUS: usize = 0;
+    const RED: usize = 1;
+    const GREEN: usize = 2;
+    const BLUE: usize = 3;
+    const CYAN: usize = 4;
+    const MAGENTA: usize = 5;
+    const YELLOW: usize = 6;
+    const ORANGE: usize = 7;
+    const PURPLE: usize = 8;
+}
+
+impl Pixel for BiomePixel {
+    type Subpixel = f32;
+
+    const CHANNEL_COUNT: u8 = 10;
+    const COLOR_MODEL: &'static str = "BIOME";
+
+    fn channels(&self) -> &[Self::Subpixel] {
+        &self.0
+    }
+
+    fn channels_mut(&mut self) -> &mut [Self::Subpixel] {
+        &mut self.0
+    }
+
+    fn channels4(
+        &self,
+    ) -> (
+        Self::Subpixel,
+        Self::Subpixel,
+        Self::Subpixel,
+        Self::Subpixel,
+    ) {
+        let mut channels = [Self::Subpixel::DEFAULT_MAX_VALUE; 4];
+        channels[0..Self::CHANNEL_COUNT as usize].copy_from_slice(&self.0);
+        (channels[0], channels[1], channels[2], channels[3])
+    }
+
+    fn from_channels(
+        a: Self::Subpixel,
+        b: Self::Subpixel,
+        c: Self::Subpixel,
+        d: Self::Subpixel,
+    ) -> Self {
+        *<Self as Pixel>::from_slice(&[a, b, c, d][..Self::CHANNEL_COUNT as usize])
+    }
+
+    fn from_slice(slice: &[Self::Subpixel]) -> &Self {
+        assert_eq!(slice.len(), Self::CHANNEL_COUNT as usize);
+        unsafe { &*(slice.as_ptr() as *const Self) }
+    }
+
+    fn from_slice_mut(slice: &mut [Self::Subpixel]) -> &mut Self {
+        assert_eq!(slice.len(), Self::CHANNEL_COUNT as usize);
+        unsafe { &mut *(slice.as_mut_ptr() as *mut Self) }
+    }
+
+    fn to_rgb(&self) -> Rgb<Self::Subpixel> {
+        Rgb([0.0, 0.0, 0.0])
+    }
+
+    fn to_rgba(&self) -> Rgba<Self::Subpixel> {
+        Rgba([0.0, 0.0, 0.0, 0.0])
+    }
+
+    fn to_luma(&self) -> Luma<Self::Subpixel> {
+        Luma([0.0])
+    }
+
+    fn to_luma_alpha(&self) -> LumaA<Self::Subpixel> {
+        LumaA([0.0, 0.0])
+    }
+
+    fn map<F>(&self, f: F) -> Self
+    where
+        F: FnMut(Self::Subpixel) -> Self::Subpixel,
+    {
+        let mut this = (*self).clone();
+        this.apply(f);
+        this
+    }
+
+    fn apply<F>(&mut self, mut f: F)
+    where
+        F: FnMut(Self::Subpixel) -> Self::Subpixel,
+    {
+        for v in &mut self.0 {
+            *v = f(*v)
+        }
+    }
+
+    fn map_with_alpha<F, G>(&self, f: F, g: G) -> Self
+    where
+        F: FnMut(Self::Subpixel) -> Self::Subpixel,
+        G: FnMut(Self::Subpixel) -> Self::Subpixel,
+    {
+        let mut this = (*self).clone();
+        this.apply_with_alpha(f, g);
+        this
+    }
+
+    fn apply_with_alpha<F, G>(&mut self, mut f: F, mut g: G)
+    where
+        F: FnMut(Self::Subpixel) -> Self::Subpixel,
+        G: FnMut(Self::Subpixel) -> Self::Subpixel,
+    {
+        let alpha = Self::CHANNEL_COUNT as usize;
+        for v in self.0[..alpha].iter_mut() {
+            *v = f(*v)
+        }
+        if let Some(v) = self.0.get_mut(alpha) {
+            *v = g(*v)
+        }
+    }
+
+    fn map2<F>(&self, other: &Self, f: F) -> Self
+    where
+        F: FnMut(Self::Subpixel, Self::Subpixel) -> Self::Subpixel,
+    {
+        let mut this = (*self).clone();
+        this.apply2(other, f);
+        this
+    }
+
+    fn apply2<F>(&mut self, other: &Self, mut f: F)
+    where
+        F: FnMut(Self::Subpixel, Self::Subpixel) -> Self::Subpixel,
+    {
+        for (a, &b) in self.0.iter_mut().zip(other.0.iter()) {
+            *a = f(*a, b)
+        }
+    }
+
+    fn invert(&mut self) {}
+
+    fn blend(&mut self, other: &Self) {
+        *self = *other;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use bevy::{color::palettes::css::GREEN, prelude::*};
+use std::f32::consts::FRAC_PI_2;
 
-use crate::level::{Level, LevelPartBuilder, PartAlign};
+use bevy::prelude::*;
+use bevy_flycam::PlayerPlugin;
+
+use crate::level::{Level, LevelBiome, LevelPartBuilder, PartAlign};
 
 mod level;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(PlayerPlugin)
         .add_systems(Startup, setup)
         .run();
 }
@@ -20,25 +24,26 @@ fn setup(
     let mut level = Level::new();
     let id = level.add(
         Vec2::ZERO,
-        LevelPartBuilder::new()
+        LevelPartBuilder::new(LevelBiome::Red)
             .with_size(20.0, 20.0)
-            .with_count(20)
+            .with_count(5)
             .with_fill_ratio(0.2)
             .build(),
     );
+    let id0 = id;
     level.add_after(
         id,
         PartAlign::Left,
-        LevelPartBuilder::new()
+        LevelPartBuilder::new(LevelBiome::Green)
             .with_size(5.0, 20.0)
-            .with_count(30)
+            .with_count(20)
             .with_fill_ratio(0.2)
             .build(),
     );
     let id = level.add_after(
         id,
         PartAlign::Right,
-        LevelPartBuilder::new()
+        LevelPartBuilder::new(LevelBiome::Blue)
             .with_size(10.0, 5.0)
             .with_count(10)
             .with_fill_ratio(0.2)
@@ -47,19 +52,88 @@ fn setup(
     level.add_after(
         id,
         PartAlign::Down,
-        LevelPartBuilder::new()
+        LevelPartBuilder::new(LevelBiome::Cyan)
             .with_size(5.0, 5.0)
             .with_count(5)
             .with_fill_ratio(0.2)
             .build(),
     );
+    let id = level.add_after(
+        id0,
+        PartAlign::Down,
+        LevelPartBuilder::new(LevelBiome::Magenta)
+            .with_size(20.0, 20.0)
+            .with_count(25)
+            .with_fill_ratio(0.2)
+            .build(),
+    );
+    let id = level.add_after(
+        id,
+        PartAlign::Down,
+        LevelPartBuilder::new(LevelBiome::Yellow)
+            .with_size(20.0, 20.0)
+            .with_count(25)
+            .with_fill_ratio(0.2)
+            .build(),
+    );
+    let id = level.add_after(
+        id,
+        PartAlign::Down,
+        LevelPartBuilder::new(LevelBiome::Orange)
+            .with_size(20.0, 20.0)
+            .with_count(25)
+            .with_fill_ratio(0.2)
+            .build(),
+    );
+    let id = level.add_after(
+        id,
+        PartAlign::Down,
+        LevelPartBuilder::new(LevelBiome::Purple)
+            .with_size(20.0, 20.0)
+            .with_count(25)
+            .with_fill_ratio(0.2)
+            .build(),
+    );
+    level.add_after(
+        id,
+        PartAlign::Down,
+        LevelPartBuilder::new(LevelBiome::Red)
+            .with_size(20.0, 20.0)
+            .with_count(25)
+            .with_fill_ratio(0.2)
+            .build(),
+    );
 
-    for point in level.points() {
+    let terrain = meshes.add(level.terrain(32.0));
+
+    let mut material = StandardMaterial::from_color(Color::WHITE);
+    material.double_sided = true;
+    material.cull_mode = None;
+
+    println!("{:?}", level.bounds().center());
+
+    commands.spawn((
+        Mesh3d(terrain),
+        MeshMaterial3d(materials.add(material)),
+        Transform::from_translation(level.bounds().center().extend(0.0).xzy())
+            .with_scale(level.bounds().size().extend(1.0))
+            .with_rotation(Quat::from_rotation_x(-FRAC_PI_2)),
+    ));
+
+    for (part, point) in level.points() {
         commands.spawn((
             Mesh3d(meshes.add(Circle::new(0.25))),
-            MeshMaterial3d(materials.add(Color::WHITE)),
-            Transform::from_xyz(point.x as f32, 0.0, point.y as f32)
-                .with_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+            MeshMaterial3d(materials.add(Color::BLACK)),
+            Transform::from_xyz(point.x, 0.02, point.y)
+                .with_rotation(Quat::from_rotation_x(-FRAC_PI_2)),
+        ));
+        commands.spawn((
+            PointLight {
+                intensity: 2000.0 * part.radius.powf(3.0),
+                range: part.radius * 2.0,
+                ..Default::default()
+            },
+            Transform::from_xyz(point.x, part.radius, point.y),
         ));
     }
 
@@ -68,35 +142,10 @@ fn setup(
         let distance = start.distance(end);
         commands.spawn((
             Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::new(1.0, 0.05)))),
-            MeshMaterial3d(materials.add(Color::WHITE)),
-            Transform::from_xyz(mid.x as f32, 0.0, mid.y as f32)
+            MeshMaterial3d(materials.add(Color::BLACK)),
+            Transform::from_xyz(mid.x as f32, 0.01, mid.y as f32)
                 .with_scale(Vec3::new(distance * 0.5, 1.0, 1.0))
                 .with_rotation(Quat::from_rotation_y((end - start).angle_to(Vec2::X))),
         ));
     }
-
-    for bounds in level.bounds() {
-        let center = bounds.center();
-        let center = Vec3::new(center.x, -0.01, center.y);
-        commands.spawn((
-            Mesh3d(meshes.add(Plane3d::new(Vec3::Y, bounds.size() * 0.5))),
-            MeshMaterial3d(materials.add(StandardMaterial::from_color(GREEN))),
-            Transform::from_translation(center),
-        ));
-    }
-
-    // light
-    commands.spawn((
-        DirectionalLight {
-            illuminance: 1000.0,
-            ..Default::default()
-        },
-        Transform::default().looking_to(-Vec3::Y, Vec3::Y),
-    ));
-
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 40.0, 0.0).looking_at(Vec3::ZERO, -Vec3::Z),
-    ));
 }
